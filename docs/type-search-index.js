3eb9d96a03a58eaab342fcceaca8e279abd87e90
ppvictoriaaa <pekurvika@gmail.com>
2024-11-07 15:31:12 +0200

    added docs folder

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Clothes"},{"p":"org.example","l":"ClothesGenerator"},{"p":"org.example","l":"CountingOutliers"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"StatisticCollector"},{"p":"org.example","l":"StatisticForPriceData"}];updateSearchResults();
